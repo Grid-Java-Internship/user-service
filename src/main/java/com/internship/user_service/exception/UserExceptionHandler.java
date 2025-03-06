076bd1ac665c8ed9566a6b26fda3a1ca4d952bb3
matijaaz <kreckoviccc2001@gmail.com>
2025-03-06 11:23:52 +0100

    add exception handling

--- src/main/java/com/internship/user_service/exception/UserExceptionHandler.java
+++ src/main/java/com/internship/user_service/exception/UserExceptionHandler.java
@@ -1,7 +1,27 @@
 package com.internship.user_service.exception;
 
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
 
 @ControllerAdvice
 public class UserExceptionHandler {
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ExceptionResponse> handleConstraintViolationException(ConstraintViolationException ex) {
+        List<String> errorMessages = ex.getConstraintViolations().stream()
+                .map(ConstraintViolation::getMessage)
+                .toList();
+
+        ExceptionResponse errorResponse = ExceptionResponse.builder()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .messages(errorMessages)
+                .build();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
 }
