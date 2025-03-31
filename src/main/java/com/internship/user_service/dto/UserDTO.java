62acb8a073e69d830e8157b081b590cab2fd087b
Milan Stošić <milanstosic00@gmail.com>
2025-03-31 10:03:15 +0200

    Added more info about user's address

--- src/main/java/com/internship/user_service/dto/UserDTO.java
+++ src/main/java/com/internship/user_service/dto/UserDTO.java
@@ -44,6 +44,18 @@ public class UserDTO {
     @Size(min = 2, max = 30, message = "Address length must be between {min} and {max} characters.")
     private String address;
 
+    @NotBlank(message = "City is mandatory.")
+    @Size(min = 2, max = 30, message = "City length must be between {min} and {max} characters.")
+    private String city;
+
+    @NotBlank(message = "Zip code is mandatory.")
+    @Size(min = 2, max = 30, message = "Zip code length must be between {min} and {max} characters.")
+    private String zipCode;
+
+    @NotBlank(message = "Country is mandatory.")
+    @Size(min = 2, max = 30, message = "Country length must be between {min} and {max} characters.")
+    private String country;
+
     private LocalDate created;
 
     private Boolean verified;
