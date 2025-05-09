e62f77784cc6b28b77bda57bed35b4770d34f225
Milan Stošić <milanstosic00@gmail.com>
2025-05-09 14:40:35 +0200

    Small fix

--- src/main/java/com/internship/user_service/service/impl/UserServiceImpl.java
+++ src/main/java/com/internship/user_service/service/impl/UserServiceImpl.java
@@ -20,6 +20,8 @@ import jakarta.transaction.Transactional;
 import com.internship.user_service.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
